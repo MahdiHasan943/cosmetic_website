71dbae8b9185284fe80b8bcad94bd64e29e643c1
MahdiHasan943 <mahdifreelancer41@gmail.com>
2023-07-11 21:35:59 +0600

    navbar complete

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,12 +1,11 @@
 "use client"
 import React, { useState, useEffect } from "react";
-
 import Link from "next/link";
-
 function Nav() {
   const [active, setActive] = useState("nav__menu");
   const [icon, setIcon] = useState("nav__toggler");
  
+  const [color,setColor] =useState(false)
 
   
   const navToggle = () => {
@@ -19,30 +18,141 @@ function Nav() {
       setIcon("nav__toggler toggle");
     } else setIcon("nav__toggler");
   };
+  const menu = (
+    <React.Fragment>
+      <li className=" group  transition-all duration-300 ease-in-out nav__item item">
+        <a
+          href="#hero"
+          onClick={() => {
+            navToggle();
+            setColor(true);
+          }}
+          className={`${
+            color === true
+              ? "text-cyan-500   bg-left-bottom bg-gradient-to-t from-cyan-500 to-blue-500     pb-3  md:pb-5       bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
+              : "text-white md:text-[#313131]"
+          } nav__link`}
+        >
+          Home
+        </a>
+      </li>
+
+      <li className="group  transition-all duration-300 ease-in-out nav__item item nav__item">
+        <a
+          href="#Services"
+          onClick={() => {
+            navToggle();
+            setColor("p");
+          }}
+          className={`${
+            color === "p"
+              ? "text-cyan-500      bg-left-bottom bg-gradient-to-t from-cyan-500 to-blue-500     pb-3  md:pb-5       bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
+              : "text-white md:text-[#313131]"
+          } nav__link`}
+        >
+         Our Services
+        </a>
+      </li>
+      <li className="group  transition-all duration-300 ease-in-out  nav__item">
+        <a
+          href="#About"
+          onClick={() => {
+            navToggle();
+            setColor("c");
+          }}
+          className={`${
+            color === "c"
+              ? "text-cyan-500           bg-left-bottom bg-gradient-to-t from-cyan-500 to-blue-500     pb-3  md:pb-5       bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
+              : "text-white md:text-[#313131]"
+          } nav__link`}
+        >
+        About Us
+        </a>
+      </li>
+      <li className="group  transition-all duration-300 ease-in-out nav__item item">
+        <a
+          href="#FAQs"
+          onClick={() => {
+            navToggle();
+            setColor("m");
+          }}
+          className={`${
+            color === "m"
+              ? "text-cyan-500            bg-left-bottom bg-gradient-to-t from-cyan-500 to-blue-500     pb-3  md:pb-5       bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
+              : "text-white md:text-[#313131]"
+          } nav__link`}
+        >
+        FAQs
+        </a>
+      </li>
+      <li className="group  transition-all duration-300 ease-in-out nav__item item">
+        <a
+          href="#Contact"
+          onClick={() => {
+            navToggle();
+            setColor("people");
+          }}
+          className={`${
+            color === "people"
+              ? "text-cyan-500              bg-left-bottom bg-gradient-to-t from-cyan-500 to-blue-500     pb-3  md:pb-5       bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
+              : "text-white md:text-[#313131]"
+          } nav__link`}
+        >
+         Contact Us
+        </a>
+      </li>
+    </React.Fragment>
+  );
 
   return (
-    <nav className=" shadow-none w-[100vw] bg-[black] py-8 px-6 lg:px-12 nav">
-      <div className="flex w-full shadow-none bg-[black]  justify-between md:justify-around ">
+    <nav className={`sm:px-16 px-6 py-3 shadow-none w-[100vw] nav`}>
+      <div className={`flex w-full items-center  md:justify-between `}>
         <div className="flex overflow-hidden  items-center">
           <a
-             href={"/"}
-            className="lg:ml-[80px]  text-blue-500 shadow-none nav__brand PB"
+             
+            className=" shadow-none nav__brand PB"
           >
-            PASSION BLEND BAR
+           <img className="w-[83px] h-[102px]" src='/images/logo.webp' alt="logo" />
           </a>
         </div>
+        <div className="">       
+<form className="hidden xl:block">   
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative">
+        <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+        </div>
+        <input type="search" id="default-search" class="block px-4 w-full py-2 outline-none  focus:outline:none  bg-[#F5F5F5] rounded-full" placeholder="" required/>
+        {/* <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button> */}
+    </div>
+</form>
 
-        <ul className={`2xl:ml-[200px] ${active}`}>
+        </div>
+
+        <ul className={` ${active}`}>
         <a
              href={"/"}
-            className="border border-white py-10 px-8 rounded-xl MAINLOGO text-blue-500 shadow-none nav__brand PB"
+            className="menu_img shadow-none nav__brand PB"
           >
-            PASSION BLEND BAR
+           <img className="w-[83px] h-[102px]" src='/images/logo.webp' alt="logo" />
           </a>
+          {menu}
 
          
         </ul>
         
+        <div className="px-2 flex gap-4 items-center justify-between">
+          <button className="hidden lg:block font-lato  bg-[#313131] rounded-lg text-white px-3 py-2 text-[13px]">
+            Book Now
+          </button>
+          <div className="hidden nm:block">
+          <img className="inline" src="/images/whatsapp.svg" alt="whatsapp" />
+          <span className="font-lato">00 44 (0)  7539 466 779</span>
+          </div>
+
+        </div>
       </div>
 
       <div onClick={navToggle} className={icon}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,27 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+
     extend: {
+
+      screens: {
+        'gm': '526px',
+        'nm':'895px',
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+         
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
       fontFamily: {
         lato: ['Lato', 'sans-serif'],
         volkhov: ['Volkhov', 'serif'],
