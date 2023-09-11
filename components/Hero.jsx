f18ae37ccf10ecf932ce352bfd34001c3490e83a
MahdiHasan943 <mahdifreelancer41@gmail.com>
2023-09-11 20:23:58 +0600

    change texts

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,17 +8,18 @@ const Hero = () => {
   <motion.div id="#hero" variants={textVariant2} initial="hidden" whileInView="show" className="lg:flex overflow-hidden justify-between 2xl:justify-around gap-10 items-center">
       <div className="py-10">
         <motion.h1 variants={textVariant2} initial="hidden" whileInView="show" className="heroTitle font-volkhov">
-          IWO Cosmetic <br className="hidden sm:block" /> Product Safety
+          IWO <br className="hidden sm:block" /> Cosmetic <br className="hidden sm:block" />  Product <br className="hidden sm:block" /> Safety <br className="hidden sm:block" />Ltd
         </motion.h1>
-        <motion.p variants={textVariant2} initial="hidden" whileInView="show" className="font-lato herosubT">
+          <motion.p variants={textVariant2} initial="hidden" whileInView="show" className="font-lato pt-3 herosubT">
+
           is a consultancy business with specialism mainly{" "}
-          <br className="hidden sm:block" /> in the fields of cosmetics and
-          chemical consumer <br className="hidden sm:block" />
-          products regulation and safety.
+          <br className="hidden sm:block" /> in the fields of regulation and safety for cosmetics <br className="hidden sm:block" />
+          and chemical consumer
+          products regulation and  <br className="hidden sm:block" /> safety.
               </motion.p>
               <div className="py-6  relative">
                   <button className="absolute font-lato bg-[#313131] text-white rounded-full py-3 px-8">Get in touch</button>
-                  <div className="ml-10 gm:flex   items-center gap-4">
+                  <div className="ml-10 gm:flex   items-center justify-end gap-4">
                   <img src="/images/icon-park.webp" alt="" />
 
                       <img src="/images/sizar.webp" alt="sizar" />
